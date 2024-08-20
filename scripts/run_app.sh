2c74e861e3ee6ad1f5ab4afaef325d6801e4f0d3
TenergyMX <tenergy.mx@gmail.com>
2024-08-20 00:53:12 -0400

    add aws repo credentials

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/test-sia
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 903371078608.dkr.ecr.us-east-2.amazonaws.com
 docker compose -f docker-compose.prod.yaml up --build -d
