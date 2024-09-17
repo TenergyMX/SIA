13940282481ab461cc487da41247eb4273bc08c8
TenergyMX <tenergy.mx@gmail.com>
2024-09-17 18:08:00 -0400

    comment remove image command undo

--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/test-sia
-#docker compose -f docker-compose.prod.yaml down
-#docker image remove 903371078608.dkr.ecr.us-east-2.amazonaws.com/sia-app
+docker compose -f docker-compose.prod.yaml down
