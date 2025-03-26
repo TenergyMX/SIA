5a2d8544c86f8236075db3da90dd1b24c9ebe4c8
Salinas27 <bryan.salinas@tenergy.con.mx>
2025-03-26 08:51:50 -0600

    Funciones con try

--- modules/views/computer-equipment/views.py
+++ modules/views/computer-equipment/views.py
@@ -1847,70 +1847,62 @@ def generar_auditoria_de_equipo_de_computo(company_id):
 
 
 
-#función para generar el código qr
 def generate_qr_computer(request, qr_type, computerSystemId):
-    context = user_data(request)
-    company_id = context["company"]["id"]
-    computer = get_object_or_404(ComputerSystem, id=computerSystemId)
+    try:
+        context = user_data(request)
+        company_id = context["company"]["id"]
+        computer = get_object_or_404(ComputerSystem, id=computerSystemId)
 
-    print(f"Generando QR para el equipo con ID: {computerSystemId}")
+        print(f"Generando QR para el equipo con ID: {computerSystemId}")
 
+        if qr_type == "info" and computer.qr_info_computer:
+            qr_url_info = generate_presigned_url(AWS_BUCKET_NAME, str(computer.qr_info_computer))
+            print(f"QR ya generado. URL: {qr_url_info}")
+            return JsonResponse({'status': 'generados', 'qr_url_info': qr_url_info})
+
+        if qr_type == 'info':
+            qr_content = f"https://sia-tenergy.com/computers-equipment/info/{computerSystemId}/"
+        else:
+            return JsonResponse({'status': 'error', 'message': 'Invalid QR type'}, status=400)
+
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(qr_content)
+        qr.make(fit=True)
+        img = qr.make_image(fill='black', back_color='white')
+
+        buffer = BytesIO()
+        img.save(buffer, format="PNG")
+        buffer.seek(0)
+
+        s3Path = f'docs/{company_id}/computers-equipment/{computerSystemId}/qr/'
+        s3Name = f"qr_info{computerSystemId}.png"
+        full_s3_path = s3Path + s3Name
+
+        print(f"Subiendo el QR a S3 en la ruta: {full_s3_path}")
+        computer.qr_info_computer = full_s3_path
+
+        img_file = InMemoryUploadedFile(
+            buffer, None, s3Name, 'image/png', buffer.getbuffer().nbytes, None
+        )
+
+        upload_to_s3(img_file, AWS_BUCKET_NAME, full_s3_path)
+        computer.save()
+
+        print(f"QR guardado en la base de datos con URL: {full_s3_path}")
+        qr_url = generate_presigned_url(AWS_BUCKET_NAME, full_s3_path)
+        print(f"Generando URL del QR en S3: {qr_url}")
+
+        return JsonResponse({'status': 'success', 'qr_url': qr_url})
     
-    if qr_type == "info" and computer.qr_info_computer:
-        qr_url_info = generate_presigned_url(AWS_BUCKET_NAME, str(computer.qr_info_computer))
-        print(f"QR ya generado. URL: {qr_url_info}")
+    except Exception as e:
+        print(f"Error al generar el QR: {e}")
+        return JsonResponse({'status': 'error', 'message': str(e)}, status=500)
 
-        return JsonResponse({'status': 'generados', 'qr_url_info': qr_url_info})
-
-    # Contenido del QR
-    if qr_type == 'info':
-        qr_content = f"https://sia-tenergy.com/computers-equipment/info/{computerSystemId}/"
-    else:
-        return JsonResponse({'status': 'error', 'message': 'Invalid QR type'}, status=400)
-
-    # Generar el QR
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(qr_content)
-    qr.make(fit=True)
-
-    img = qr.make_image(fill='black', back_color='white')
-
-    # Guardar la imagen en un buffer
-    buffer = BytesIO()
-    img.save(buffer, format="PNG")
-    buffer.seek(0)
-
-    # Definir la ruta del archivo en S3
-    s3Path = f'docs/{company_id}/computers-equipment/{computerSystemId}/qr/'
-    s3Name = f"qr_info{computerSystemId}.png"
-    full_s3_path = s3Path + s3Name
-
-    print(f"Subiendo el QR a S3 en la ruta: {full_s3_path}")
-
-    # Asignar la ruta al modelo
-    computer.qr_info_computer = full_s3_path
-
-    # Crear un InMemoryUploadedFile
-    img_file = InMemoryUploadedFile(
-        buffer, None, s3Name, 'image/png', buffer.getbuffer().nbytes, None
-    )
-
-    # Subir a S3
-    upload_to_s3(img_file, AWS_BUCKET_NAME, full_s3_path)
-    computer.save()  # Guardar cambios en la base de datos
-
-    print(f"QR guardado en la base de datos con URL: {full_s3_path}")
-
-    # Generar la URL 
-    qr_url = generate_presigned_url(AWS_BUCKET_NAME, full_s3_path)
-    print(f"Generando URL del QR en S3: {qr_url}")
-
-    return JsonResponse({'status': 'success', 'qr_url': qr_url})
 
 
 def check_qr_computer(request, computerSystemId):
@@ -1929,23 +1921,28 @@ def check_qr_computer(request, computerSystemId):
 
 #funcion para descargar el qr
 def descargar_qr_computer(request):
-    id_computer = request.GET.get("computerSystemId")
-    tipo_qr = request.GET.get("type")  
+    try:
+        id_computer = request.GET.get("computerSystemId")
+        tipo_qr = request.GET.get("type")  
 
-    computer = ComputerSystem.objects.filter(id=id_computer).first()
-    if computer:
+        computer = ComputerSystem.objects.filter(id=id_computer).first()
+        if not computer:
+            return JsonResponse({'error': 'Equipo no encontrado'}, status=404)
+
         if tipo_qr == "info":
             if not computer.qr_info_computer:
                 return JsonResponse({'error': 'QR no generado'}, status=404)
-
+            
             url_computer = computer.qr_info_computer  
         else:
             return JsonResponse({'error': 'Tipo de QR no válido'}, status=400)
 
         url_s3 = generate_presigned_url(AWS_BUCKET_NAME, str(url_computer))
         return JsonResponse({'url_computer': url_s3})
-    else:
-        return JsonResponse({'error': 'Equipo no encontrado'}, status=404)
+    
+    except Exception as e:
+        print(f"Error al descargar el QR: {e}")
+        return JsonResponse({'error': str(e)}, status=500)
 
 
 #funcion para eliminar el qr
