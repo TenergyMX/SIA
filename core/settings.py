02917b3dcdbda5142d99e2ff8951f0df72f40f05
TenergyMX <tenergy.mx@gmail.com>
2025-04-17 12:43:33 -0400

    add abspath settings

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from pathlib import Path
 import os
-from os.path import join, dirname
+from os.path import join, dirname, abspath
 from dotenv import load_dotenv
 
 from django.conf import settings
